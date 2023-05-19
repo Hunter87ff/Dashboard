027699b653cb46d39b6f35a0a888495d8d66f191
Hunter87ff <83773586+Hunter87ff@users.noreply.github.com>
2023-05-19 19:32:08 +0000

    update

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ from pymongo import MongoClient
 sdb = MongoClient(os.environ["spdb"])
 sdbc = sdb["qna"]["query"]
 value = os.environ["token"]
-docs = sdbc.find()
 
+
+docs = list(sdbc.find())
 
 
 app = Flask('app')
@@ -29,7 +30,7 @@ def login():
 def oauth():
 	data = request.form.to_dict()
 	print(data)
-	token = secrets.token_hex(16)
+	#token = secrets.token_hex(16)
 	if data["email"] != os.environ["amail"]:
 		return "You've Entered Wrong Email"
 	if data["pass"] != os.environ["apass"]:
@@ -52,11 +53,25 @@ def verify():
 		return "<script>window.location.href='https://dbm.sourav87.repl.co/dashboard'</script>"
 	return "<script>window.location.href='https://dbm.sourav87.repl.co/login'</script>"
 		
+@app.route("/add", methods=["POST"])
+def add():
+	data = request.form.to_dict()
+	db = sdbc.find_one({"q":data["q"]})
+	if db != None:
+		sdbc.update_one({"q":data["q"]},{"$set":data})
+	if db == None:
+		sdbc.insert_one(data)
+	docs = list(sdbc.find())
+	return "<script>window.location.href='https://dbm.sourav87.repl.co/dashboard'</script>"
 
-	
+
+@app.route("/docs")
+def doc():
+	return render_template("docs.html", list=docs, token=value)
+
 @app.route("/dashboard")
 def dash():
-	data =  {"docs":len(list(docs))}
+	data =  {"docs":len(docs)}
 	return render_template("index.html", token=value, list=docs, data=data)
 	
 
